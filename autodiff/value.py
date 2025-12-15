b7fc16fafcb190f60d672f3c8c8dbe19a5bdade4
Suryansh <surushakya@proton.me>
2025-12-15 21:09:11 +0530

    feat: more ops for Value

--- autodiff/value.py
+++ autodiff/value.py
@@ -32,6 +32,17 @@ class Value:
         
         return out
 
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -57,3 +68,24 @@ class Value:
         # Running backrop in reverse topological manner
         for node in reversed(topo):
             node._backward()
+    
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
